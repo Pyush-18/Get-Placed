569bb52afe990987fda075f0d08281f20f53af39
pyush-18 <piyushjoshi1812@gmail.com>
2025-02-27 20:47:11 +0530

    fixing bugs

--- frontend/src/components/shared/ProtectedForCompanyRoute.jsx
+++ frontend/src/components/shared/ProtectedForCompanyRoute.jsx
@@ -4,7 +4,7 @@ import { Navigate } from 'react-router-dom'
 
 function ProtectedForCompanyRoute({element}) {
     const {authUser} = useSelector(store => store.auth)
-    if(authUser.role === "student"){
+    if(authUser?.role === "student"){
         return <h2 className='text-red-500 font-bold text-8xl text-center mt-10'>Access Denied : This page is only for recruiter</h2>
     }
   return authUser && authUser?.role === "recruiter" ? element : <Navigate to="/login" />
--- frontend/src/components/shared/ProtectedForStudentRoute.jsx
+++ frontend/src/components/shared/ProtectedForStudentRoute.jsx
@@ -4,7 +4,7 @@ import { Navigate } from "react-router-dom";
 function ProtectedForStudentRoute({ element }) {
   const { authUser } = useSelector((store) => store.auth);
 
-  if(authUser.role === "recruiter"){
+  if(authUser?.role === "recruiter"){
     return <h2 className='text-red-500 font-bold text-center mt-5'>Access Denied : This page is only for student</h2>
 }
   return authUser && authUser?.role === "student"  ? element : <Navigate to="/login" replace />
